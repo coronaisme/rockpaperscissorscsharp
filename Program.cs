3b12f155bef57092d84823ae196f6022fb5b2a87
coronaisme <coronaisme@gmail.com>
2021-02-10 12:23:31 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose R for Rock, P for Paper, or S for Scissors");
+        }
+    }
+}
