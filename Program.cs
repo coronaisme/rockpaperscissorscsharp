9dd3f71f536794c1d8b59e36ed9b969a1a1c711d
coronaisme <coronaisme@gmail.com>
2021-02-10 12:48:21 -0800

    working logic

--- Program.cs
+++ Program.cs
@@ -6,14 +6,44 @@ namespace rockPaperScissors
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Choose R for Rock, P for Paper, or S for Scissors");
-            string user = Console.ReadLine();
+            string userInput = Console.ReadLine();
 
             string[] choices = new [] { "R", "P", "S" };
             Random randomChoice = new Random();
-            int compChoice = randomChoice.Next(0, choices.Length);
-            Console.WriteLine(choices[compChoice]);
-        
+            int compChooses = randomChoice.Next(0, choices.Length);
+            string compChoice = choices[compChooses];
+            Console.WriteLine("The computer chose " + choices[compChooses]);
+
+            
+            if(compChoice == "R") {
+                if(userInput == "R") {
+                    Console.WriteLine("Tie Game!");
+                } else if(userInput == "P") {
+                    Console.WriteLine("Nice Job, You Win!");
+                } else if(userInput == "S") {
+                    Console.WriteLine("You Lose! HAHA");
+                }
+            } else if(compChoice == "P") {
+                if(userInput == "R") {
+                    Console.WriteLine("You Lose! HAHA");
+                } else if(userInput == "P") {
+                    Console.WriteLine("Tie Game!");
+                } else if(userInput == "S") {
+                    Console.WriteLine("Nice Job, You Win!");
+                }
+            } else if(compChoice == "S") {
+                if(userInput == "R") {
+                    Console.WriteLine("Nice Job, You Win!");
+                } else if(userInput == "P") {
+                    Console.WriteLine("You Lose! HAHA");
+                } else if(userInput == "S") {
+                    Console.WriteLine("Tie Game");
+                }
+            }
+
+
         
         }
     }
