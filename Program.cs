2e82b89274a3df9ce9162d3c763e047b9b7ca55c
coronaisme <coronaisme@gmail.com>
2021-02-10 12:33:41 -0800

    randomizer

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace rockPaperScissors
         static void Main(string[] args)
         {
             Console.WriteLine("Choose R for Rock, P for Paper, or S for Scissors");
+            string user = Console.ReadLine();
+
+            string[] choices = new [] { "R", "P", "S" };
+            Random randomChoice = new Random();
+            int compChoice = randomChoice.Next(0, choices.Length);
+            Console.WriteLine(choices[compChoice]);
+        
+        
         }
     }
 }
